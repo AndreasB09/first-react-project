a98e3cc97b44aa5c2450179545a426b04a791024
andreasb09 <andreasb09.kodehode@gmail.com>
2024-02-05 12:41:05 +0100

    added logo and link

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,14 @@ import './App.css'
 
 function App() {
 
-  return (<h2>Hello World</h2>)
+  return (
+    <div>
+      <h1>Hey Universe</h1>
+      <a href="https://github.com/AndreasB09">
+        <img src="./images/github_logo.svg" alt="github logo" id="github"></img>
+      </a>
+    </div>
+  );
 }
 
 export default App
